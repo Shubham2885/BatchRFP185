5ba3001c159cb4c36c1adc41383222d745684f24
Shubham Chavan <48453880+Shubham2885@users.noreply.github.com>
2022-08-06 09:26:08 +0530

    Update HelloWorld.java
    changes adding for conflic from github purpose

--- src/firstpack/HelloWorld.java
+++ src/firstpack/HelloWorld.java
@@ -5,5 +5,6 @@ public class HelloWorld {
 	public static void main(String args[]) {
 		System.out.println("HelloWorld...");
 		System.out.println("changes adding for pulling purpose...");
+		System.out.println("changes adding for conflic from github purpose...");
 	}
 }
