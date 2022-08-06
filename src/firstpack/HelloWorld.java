3ceadf117c4ede11f555aa396be528fa8bf11ebc
Shubham2885 <shubham.chavan689977@gmail.com>
2022-08-06 09:27:02 +0530

    conflic changes

--- src/firstpack/HelloWorld.java
+++ src/firstpack/HelloWorld.java
@@ -4,6 +4,8 @@ public class HelloWorld {
 
 	public static void main(String args[]) {
 		System.out.println("HelloWorld...");
+		System.out.println("changes adding for conflic purpose...");
 		System.out.println("changes adding for pulling purpose...");
+		
 	}
 }
